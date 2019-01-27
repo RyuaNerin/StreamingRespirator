9106def2ee819cb448d33ad0cac0cbfc60c72fd0
RyuaNerin <admin@ryuanerin.kr>
2019-01-27 21:52:51 +0900

    PacketFriends 디버그 정보 추가

--- StreamingRespirator/Core/Streaming/Twitter/Packet/PacketFriends.cs
+++ StreamingRespirator/Core/Streaming/Twitter/Packet/PacketFriends.cs
@@ -1,10 +1,15 @@
+using System.Diagnostics;
 using Newtonsoft.Json;
 
 namespace StreamingRespirator.Core.Streaming.Twitter.Packet
 {
+    [DebuggerDisplay("Friends : {this.Friends?.Length}")]
     internal class PacketFriends : IPacket
     {
         [JsonProperty("friends")]
         public long[] Friends { get; set; }
+
+        public override string ToString()
+            => $"Friends : {this.Friends?.Length}";
     }
 }
