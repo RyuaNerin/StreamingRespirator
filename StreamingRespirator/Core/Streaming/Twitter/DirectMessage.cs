4e28927816b4d8f45f3dc0a31c832817be2da85f
RyuaNerin <admin@ryuanerin.kr>
2019-01-28 23:33:51 +0900

    서브클래스 안으로 이동

--- StreamingRespirator/Core/Streaming/Twitter/DirectMessage.cs
+++ StreamingRespirator/Core/Streaming/Twitter/DirectMessage.cs
@@ -9,64 +9,64 @@ namespace StreamingRespirator.Core.Streaming.Twitter
     internal class DirectMessage
     {
         [JsonProperty("user_inbox")]
-        public DirectMessageItem UserInbox { get; set; }
+        public UserItem UserInbox { get; set; }
 
         [JsonProperty("user_events")]
-        public DirectMessageItem UserEvents { get; set; }
+        public UserItem UserEvents { get; set; }
 
         [JsonIgnore]
-        public DirectMessageItem Item => this.UserEvents ?? this.UserInbox;
-    }
+        public UserItem Item => this.UserEvents ?? this.UserInbox;
 
-    [DebuggerDisplay("Entries : {Entries?.Length}")]
-    internal class DirectMessageItem
-    {
-        [JsonProperty("conversations")]
-        public object Conversations { get; set; }
+        [DebuggerDisplay("Entries : {Entries?.Length}")]
+        public class UserItem
+        {
+            [JsonProperty("conversations")]
+            public object Conversations { get; set; }
 
-        [JsonProperty("cursor")]
-        public string Cursor { get; set; }
+            [JsonProperty("cursor")]
+            public string Cursor { get; set; }
 
-        [JsonProperty("entries")]
-        public DirectMessageEntry[] Entries { get; set; }
+            [JsonProperty("entries")]
+            public Entry[] Entries { get; set; }
 
-        [JsonProperty("users")]
-        public Dictionary<string, TwitterUser> Users { get; set; }
-    }
+            [JsonProperty("users")]
+            public Dictionary<string, TwitterUser> Users { get; set; }
+        }
 
-    [DebuggerDisplay("{Message}")]
-    internal class DirectMessageEntry
-    {
-        [JsonProperty("message")]
-        public DirectMessageMessageData Message { get; set; }
-    }
+        [DebuggerDisplay("{Message}")]
+        public class Entry
+        {
+            [JsonProperty("message")]
+            public Message Message { get; set; }
+        }
 
-    [DebuggerDisplay("{Data}")]
-    internal class DirectMessageMessageData
-    {
-        [JsonProperty("message_data")]
-        public DirectMessageMessage Data { get; set; }
-    }
+        [DebuggerDisplay("{Data}")]
+        public class Message
+        {
+            [JsonProperty("message_data")]
+            public MessageData Data { get; set; }
+        }
 
-    [DebuggerDisplay("{Sender_Id} > {Recipiend_Id} : {Id} / {Text}")]
-    internal class DirectMessageMessage
-    {
-        [JsonProperty("id")]
-        public long Id { get; set; }
+        [DebuggerDisplay("{Sender_Id} > {Recipiend_Id} : {Id} / {Text}")]
+        public class MessageData
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
 
-        [JsonProperty("time")]
-        public long Time { get; set; }
+            [JsonProperty("time")]
+            public long Time { get; set; }
 
-        [JsonIgnore]
-        public DateTime CreatedAt => new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds(this.Time);
+            [JsonIgnore]
+            public DateTime CreatedAt => new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds(this.Time);
 
-        [JsonProperty("recipient_id")]
-        public string Recipiend_Id { get; set; }
+            [JsonProperty("recipient_id")]
+            public string Recipiend_Id { get; set; }
 
-        [JsonProperty("sender_id")]
-        public string Sender_Id { get; set; }
+            [JsonProperty("sender_id")]
+            public string Sender_Id { get; set; }
 
-        [JsonProperty("text")]
-        public string Text { get; set; }
+            [JsonProperty("text")]
+            public string Text { get; set; }
+        }
     }
 }
--- StreamingRespirator/Core/Streaming/Twitter/Packet/PacketDirectMessage.cs
+++ StreamingRespirator/Core/Streaming/Twitter/Packet/PacketDirectMessage.cs
@@ -8,43 +8,43 @@ namespace StreamingRespirator.Core.Streaming.Twitter.Packet
     internal class PacketDirectMessage : IPacket
     {
         [JsonProperty("direct_message")]
-        public PacketDirectMessageItem Item { get; set; } = new PacketDirectMessageItem();
+        public DirectMessageItem Item { get; set; } = new DirectMessageItem();
 
         public override string ToString()
             => $"{this.Item.Sender.ScreenName} -> {this.Item.Recipient.ScreenName} : {this.Item.Text}";
-    }
 
-    internal class PacketDirectMessageItem
-    {
-        [JsonProperty("id")]
-        public long Id { get; set; }
+        public class DirectMessageItem
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
 
-        [JsonProperty("id_str")]
-        public string IdStr => this.Id.ToString();
+            [JsonProperty("id_str")]
+            public string IdStr => this.Id.ToString();
 
-        [JsonProperty("text")]
-        public string Text { get; set; }
+            [JsonProperty("text")]
+            public string Text { get; set; }
 
-        [JsonProperty("created_at")]
-        public DateTime CreatedAt { get; set; }
-        
-        [JsonProperty("sender")]
-        public TwitterUser Sender { get; set; }
+            [JsonProperty("created_at")]
+            public DateTime CreatedAt { get; set; }
 
-        [JsonProperty("sender_id")]
-        public long SenderId => this.Sender?.Id ?? 0;
+            [JsonProperty("sender")]
+            public TwitterUser Sender { get; set; }
 
-        [JsonProperty("sender_screen_name")]
-        public string SenderScreenName => this.Sender?.ScreenName;
+            [JsonProperty("sender_id")]
+            public long SenderId => this.Sender?.Id ?? 0;
 
-        [JsonProperty("recipient")]
-        public TwitterUser Recipient { get; set; }
+            [JsonProperty("sender_screen_name")]
+            public string SenderScreenName => this.Sender?.ScreenName;
 
-        [JsonProperty("recipient_id")]
-        public long RecipientId => this.Recipient?.Id ?? 0;
+            [JsonProperty("recipient")]
+            public TwitterUser Recipient { get; set; }
 
-        [JsonProperty("recipient_screen_name")]
-        public string RecipientScreenName => this.Recipient?.ScreenName;
+            [JsonProperty("recipient_id")]
+            public long RecipientId => this.Recipient?.Id ?? 0;
 
+            [JsonProperty("recipient_screen_name")]
+            public string RecipientScreenName => this.Recipient?.ScreenName;
+
+        }
     }
 }
